390e138672d084ca7618c8899b388ca1398efce3
Paul Meng <mno2@mno2.org>
2015-01-17 21:20:24 +0800

    Add cuda package into dependency list and add /usr/local/cuda/lib64 into LD_LIBRARY_PATH

--- scripts/travis/travis_install.sh
+++ scripts/travis/travis_install.sh
@@ -3,16 +3,9 @@ set -e
 
 MAKE="make --jobs=$NUM_THREADS"
 
-# Install apt packages where the Ubuntu 12.04 default and ppa works for Caffe
-
-# This ppa is for gflags and glog
-add-apt-repository -y ppa:tuleu/precise-backports
 apt-get -y update
 apt-get install \
-    wget git curl unzip ctags \
-    python-dev \
-    libboost-dev libboost-system-dev libboost-python-dev libboost-thread-dev \
-    libatlas-dev libatlas-base-dev
+    wget git curl unzip ctags
 
 
 CUDA_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_6.5-14_amd64.deb
@@ -20,11 +13,8 @@ CUDA_FILE=/tmp/cuda_install.deb
 curl $CUDA_URL -o $CUDA_FILE
 dpkg -i $CUDA_FILE
 rm -f $CUDA_FILE
+
 apt-get -y update
-# Install the minimal CUDA subpackages required to test Caffe build.
-# For a full CUDA installation, add 'cuda' to the list of packages.
-apt-get -y install cuda-core-6-5 cuda-cublas-6-5 cuda-cublas-dev-6-5 cuda-cudart-6-5 cuda-cudart-dev-6-5 cuda-curand-6-5 cuda-curand-dev-6-5
-# Create CUDA symlink at /usr/local/cuda
-# (This would normally be created by the CUDA installer, but we create it
-# manually since we did a partial installation.)
+apt-get -y install cuda-core-6-5 cuda-cublas-6-5 cuda-cublas-dev-6-5 cuda-cudart-6-5 cuda-cudart-dev-6-5 cuda-curand-6-5 cuda-curand-dev-6-5 cuda
+
 ln -s /usr/local/cuda-6.5 /usr/local/cuda
